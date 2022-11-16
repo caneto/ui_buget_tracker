128a1b8964a4e38fec1784fe4c7e674ffeeee56e
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-11-16 10:31:14 -0300

    Atualização

--- lib/widgets/category_widget.dart
+++ lib/widgets/category_widget.dart
@@ -1,29 +1,79 @@
 import 'package:flutter/material.dart';
 
 class CategoryWidget extends StatelessWidget {
+  const CategoryWidget({Key? key}) : super(key: key);
 
-  const CategoryWidget({ Key? key }) : super(key: key);
-
-   @override
-   Widget build(BuildContext context) {
-       return Column(
-        children: [
-          GestureDetector(
-            onTap: () {
-              //Navigator.push(context, MaterialPageRoute(builder: CategoryPage()))
-            },
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              padding: const EdgeInsets.all(20),
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            //Navigator.push(context, MaterialPageRoute(builder: CategoryPage()))
+          },
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            padding: const EdgeInsets.all(20),
+            height: 100,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.white,
+                  offset: Offset(0, 2),
+                  blurRadius: 6,
+                )
+              ],
             ),
-          )
-        ],
-       );
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Text(
+                      "Comida",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      "R\$200 / R\$500",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Stack(
+                  children: [
+                    Container(
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                    Container(
+                      height: 20,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    )
+                  ],
+                )
+
+              ],
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
